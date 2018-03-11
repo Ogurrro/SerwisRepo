e69c9ed2e8a042be7180b53113239d67f685d4df
piter <piter@R2-D2>
2018-03-11 16:07:25 +0100

    dialog start

---
+++ SerwisAngular/partials/Naprawy/NaprawyCtrl.js
@@ -0,0 +1,79 @@
+﻿(function () {
+
+    'use strict';
+
+    angular
+
+        .module('myApp')
+
+        .controller('NaprawyCtrl', NaprawyCtrl);
+
+
+
+    NaprawyCtrl.$inject = ['$log', '$rootScope', '$window', '$q', '$scope', 'SerwisFactory', '$mdDialog'];
+
+
+
+    function NaprawyCtrl($log, $rootScope, $window, $q, $scope, SerwisFactory, $mdDialog) {
+
+        var vm = this;
+        
+        $scope.modelTxt = "1234567";
+
+        $scope.txtChanged = function (text) {
+            if (text.length != 0) {
+                $scope.modelTxt = text.toString().replace(/\B(?=(\d{3})+(?!\d))/g, " ");
+            }
+        }
+
+        $scope.txtChanged($scope.modelTxt);
+
+        $scope.clickLipka = function () {
+            alert("Lubię placki");
+        }
+
+
+        $scope.dodajNapraweDialog = function (ev) {
+
+            $mdDialog.show({
+
+                controller: 'AddMeetingCtrl',
+
+                templateUrl: 'partials/singleClientView/Meetings/Meeting/AddMeeting.html',
+
+                parent: angular.element(document.body),
+
+                targetEvent: ev,
+
+                clickOutsideToClose: false,
+
+                multiple: true,
+
+                skipHide: true,
+
+                locals: {
+                    //naprawaId : 
+                }
+            }).then(function (answer) {
+
+                toastr.success('Meeting added', { timeOut: 1600, positionClass: 'toast-bottom-right', showMethod: 'slideDown' });
+
+                $q.when(contactFactory.clientContactListData).then(displayerClientContacts);
+
+            }, function () {
+
+            });
+
+
+
+        };
+
+        function onError(reason) {
+
+            $log.error(reason);
+
+        }
+
+    }
+
+})()
