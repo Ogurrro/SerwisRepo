0c293b1841ce0039e2c7d54a82bbf3bf849ee115
Piotr Ogórek <piterog@live.com>
2018-05-27 22:24:48 +0200

    web service prep

---
+++ App_Code/WebService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Security.Principal;
+using System.Web.Script.Services;
+using System.Web.Services;
+
+/// <summary>
+/// Opis podsumowujący dla WebService
+/// </summary>
+public class WebService
+{
+    public WebService()
+    {
+        //
+        // TODO: Tutaj dodaj logikę konstruktora
+        //
+    }
+}
--- SerwisAngular/Web Service/Connection.cs
+++ SerwisAngular/Web Service/Connection.cs
@@ -3,5 +3,5 @@
 {
 
     //public static readonly string CONNECTIONSTRING_SerwisDB = "Data Source=R2-D2/SQLEXPRESS;Initial Catalog=Baza serwis;Integrated Security=True;Connect Timeout=99999;";
-    public static readonly string CONNECTIONSTRING_SerwisDB = "";
+    public static readonly string CONNECTIONSTRING_SerwisDB = @"Data Source=DESKTOP-7OBK5P9\SQLEXPRESS;Initial Catalog=SerwisDB;Integrated Security=True;Connect Timeout=99999";
 }
