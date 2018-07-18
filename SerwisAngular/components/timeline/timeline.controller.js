4b64b76416155ff9cc6ff114267ea31eb144456a
Piotr Ogórek <piterog@live.com>
2018-07-18 22:10:43 +0200

    fix

--- SerwisAngular/components/timeline/timeline.controller.js
+++ SerwisAngular/components/timeline/timeline.controller.js
@@ -14,8 +14,8 @@
         vm.statusList = [
             { status: 'Brak działań', inputDate: new Date(), descr: 'Prospekt z potencjałem', completed: true },
             { status: 'Wstępne rozmowy', inputDate: new Date(), descr: 'Prospekt z potencjałem', completed: true },
-            { status: 'Planowanie podpisania umowy', inputDate: new Date(), descr: 'Prospekt z potencjałem', completed: false },
-            { status: 'Umowa podpisana', inputDate: new Date(), descr: 'Prospekt z potencjałem', completed: false }
+            { status: 'Planowanie podpisania umowy', inputDate: null, descr: 'Prospekt z potencjałem', completed: false },
+            { status: 'Umowa podpisana', inputDate: null, descr: 'Prospekt z potencjałem', completed: false }
         ]
     }
 
